790d9a5cbdf026f1783a12701c7eb1627b042d45
Romi <de.meenta@gmail.com>
2023-07-08 16:04:47 +0000

    add app base

---
+++ app/fun_calculator.py
@@ -0,0 +1,13 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("No se puede dividir por cero.")
+    return x / y
